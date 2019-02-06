3e93ce333d301f2751113e1e08dae6f5ecd02163
ndeye gaye <gayeeendeyeee@gmail.com>
2019-02-06 22:56:24 +0100

    ajout de fichier du projet

---
+++ tp2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet" type="text/css" />
+<head>
+<body background=4b.jpg>
+<center>
+	<title>Contact</title>
+</head>
+<body>
+
+<BODY bgcolor='pink'>
+	<font color=pink> 
+</font>
+<big> <big> <big>
+<TABLE cellspacing=0 BORDER=4%>
+<TR> 
+<TD COLSPAN=2><center><img src= "ndeye.jpg"width=150 height=200 alt= "poudres"></TD>
+</TR>
+<TR>
+<TD><?php echo "Nom";?>:</TD>
+<TD>
+GAYE
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Prénom";?>:</TD>
+<TD>
+NDEYE
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Numéro de téléphone";?>:</TD>
+<TD>
+776355866
+</TD>
+</TR>
+<TR>
+<br><br>
+<TD><?php echo "Email";?>:</TD>
+</br></br>
+<TD>
+gayeeendeyeee@gmail.com
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Matricule";?>:</TD>
+<TD>
+201708DEM
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Adresse";?>:</TD>
+<TD>
+GRAND-DAKAR
+</TD>
+</TR>
+<TR> 
+<TD COLSPAN=2><center><INPUT type="submit" value="Envoyer"></INPUT></TD>
+</TR>
+</TABLE>
+
+</big>
+
+</center>
+
+</body>
+</html>
